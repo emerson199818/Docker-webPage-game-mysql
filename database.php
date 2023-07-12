3db87bd5a0cbbffc8b0c0eae9a8239d2c0358dc4
EmersonVip <134098023+emerson199818@users.noreply.github.com>
2023-07-11 23:43:05 -0500

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$server = '192.168.1.12';
-$username = 'admin';
-$password = '1143407053';
-$database = 'usuarios';
+$server = '192.168.1.12'; #ip servidor mysql, puerto (opcional)
+$username = 'admin'; #usuario de mysql
+$password = '1143'; #password de usuario
+$database = 'usuarios'; #nombre base de datos
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
